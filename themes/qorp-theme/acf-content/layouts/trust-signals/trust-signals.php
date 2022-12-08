85e8497db4772cda81ade69807d0c05bd102b1db
Damon Cook <damonacook@gmail.com>
2022-12-08 17:40:56 -0500

    🚀 RELEASE: v0.1.3

--- themes/qorp-theme/acf-content/layouts/trust-signals/trust-signals.php
+++ themes/qorp-theme/acf-content/layouts/trust-signals/trust-signals.php
@@ -12,7 +12,7 @@ $logos = $args['logo'];
 <div class="qorp-layouts qorp-layouts-trust-signals trust-signals" style="margin-top:var(--wp--preset--spacing--lg);margin-bottom:var(--wp--preset--spacing--lg);">
 	<div class="has-global-padding is-layout-constrained wp-block-group alignfull">
 		<div class="alignwide" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);border:var(--wp--custom--border-transparent);border-radius:var(--wp--custom--border-radius--lg);">
-			<h2 class="has-text-align-center fw-bold has-large-font-size">Trusted by leading organizations</h2>
+			<h2 class="has-text-align-center fw-bold has-large-font-size" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--md)">Trusted by leading organizations</h2>
 
 			<?php if ( $logos ) : ?>
 				<div class="is-layout-flex" style="justify-content:center;gap:var(--wp--preset--spacing--md);">

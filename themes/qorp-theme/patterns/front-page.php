4c89731b40d2851b137dc4c47af0e6df82161831
Damon Cook <damonacook@gmail.com>
2023-04-27 13:00:25 -0400

    👌 IMPROVE: sync Site Editor changes

---
+++ themes/qorp-theme/patterns/front-page.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: qorp/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","theme":"qorp-theme","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"site-content","layout":{"type":"constrained"}} -->
+<main class="wp-block-group site-content" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hyperspace-Perfect-Landing.svg","id":220,"hasParallax":true,"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light has-parallax" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background wp-image-220 has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hyperspace-Perfect-Landing.svg)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"top","align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"},"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"},"margin":{"top":"var:preset|spacing|md"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-top" style="margin-top:var(--wp--preset--spacing--md);padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:column {"verticalAlignment":"top","width":"64%","style":{"spacing":{"padding":{"top":"var:preset|spacing|md"}}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-top:var(--wp--preset--spacing--md);flex-basis:64%"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"textTransform":"none"}},"fontSize":"xx-large"} -->
+<h1 class="wp-block-heading has-text-align-left has-xx-large-font-size" style="text-transform:none">Do qui deserunt duis pariatur excepteur</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"36%","style":{"spacing":{"padding":{"top":"var:preset|spacing|md"}}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-top:var(--wp--preset--spacing--md);flex-basis:36%"><!-- wp:image {"align":"left","id":208,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image alignleft size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Hyperspace-Floating-1.svg" alt="" class="wp-image-208"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|md","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|md"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--md)"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|md","left":"var:preset|spacing|md"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"38%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:38%"><!-- wp:heading {"fontSize":"max-48"} -->
+<h2 class="wp-block-heading has-max-48-font-size">Really Engaging Marketing Ideas</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Quis ea laborum pariatur sint sint proident consequat pariatur est</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Watch Now</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"secondary","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button">Request a Demo</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"62%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:62%"><!-- wp:image {"id":12,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/placeholder.png" alt="" class="wp-image-12"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","right":"var:preset|spacing|md","bottom":"var:preset|spacing|lg","left":"var:preset|spacing|md"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--lg);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--lg);padding-left:var(--wp--preset--spacing--md)"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"62%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:62%"><!-- wp:image {"id":31,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/placeholder2.png" alt="" class="wp-image-31"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"38%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:38%"><!-- wp:heading {"fontSize":"max-48"} -->
+<h2 class="wp-block-heading has-max-48-font-size"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">99.99%</mark> Customer Satisfaction</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Est veniam velit sed deserunt consectetur deserunt deserunt dolore et exercitation sunt</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Watch Now</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Request a Demo</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","theme":"qorp-theme","tagName":"footer","className":"site-footer"} /-->
